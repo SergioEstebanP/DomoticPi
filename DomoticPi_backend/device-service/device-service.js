af9e302781ae26074f4ecdbb8f4a9b278cde6c0f
Sergio Esteban <Sergio.Esteban@externas.adidas.com>
2019-09-23 16:45:41 +0200

    bugfix

--- DomoticPi_backend/device-service/device-service.js
+++ DomoticPi_backend/device-service/device-service.js
@@ -19,7 +19,7 @@ app.set('json spaces', 2)
 
 // mysql data access
 const connection = mysql.createConnection({
-	host: 'localhost',
+	host: '0.0.0.0',
 	user: 'device-service',
 	password: 'device-service',
 	database: 'devicesDB',
