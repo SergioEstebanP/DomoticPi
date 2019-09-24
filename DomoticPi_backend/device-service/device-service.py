d5626d229f0bba937f729d657190942fc24e2a48
Sergio Esteban <Sergio.Esteban@externas.adidas.com>
2019-09-24 15:17:28 +0200

    Changed infra to python

--- DomoticPi_backend/device-service/device-service.py
+++ DomoticPi_backend/device-service/device-service.py
@@ -1,23 +1,28 @@
 import mysql.connector
+from flask import Flask
 from mysql.connector import Error
 
-try:
-    connection = mysql.connector.connect(
-            host='172.17.0.2',
-            database='devicesDB',
-            user='device-service',
-            password='device-service')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        sql_query = "select * from device"
-        cursor = connection.cursor()
-        cursor.execute(sql_query)
-        result = cursor.fetchall()
-        print("{}".format(result))
+app = Flask(__name__)
 
-except Error as e:
-    print(e)
-finally:
-    if (connection.is_connected()):
-        connection.close()
-        print("Mysql Connection CLosed")
+@app.route('/devices')
+def index():
+    try:
+        connection = mysql.connector.connect(
+                host='172.17.0.2',
+                database='devicesDB',
+                user='device-service',
+                password='device-service')
+        if connection.is_connected():
+            db_Info = connection.get_server_info()
+            sql_query = "select * from device"
+            cursor = connection.cursor()
+            cursor.execute(sql_query)
+            result = cursor.fetchall()
+            return result
+
+    except Error as e:
+        print(e)
+    finally:
+        if (connection.is_connected()):
+            connection.close()
+            print("Mysql Connection CLosed")
